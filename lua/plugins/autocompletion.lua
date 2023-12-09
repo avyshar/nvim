e678d8e046fcf5c4afc5a3ee71752b3634d572c1
Artem Vyshar <as.vyshar@gmail.com>
2023-12-09 13:23:08 +0200

    cmp-buf

--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -7,6 +7,7 @@ return {
         'saadparwaiz1/cmp_luasnip',
 
         -- Adds LSP completion capabilities
+        'hrsh7th/cmp-buffer',
         'hrsh7th/cmp-nvim-lsp',
         'hrsh7th/cmp-path',
 
@@ -67,6 +68,7 @@ return {
                 { name = "copilot", group_index = 2 },
                 -- Other Sources
                 { name = "nvim_lsp", group_index = 2 },
+                { name = "buffer", group_index = 2 },
                 { name = "path", group_index = 2 },
                 { name = "luasnip", group_index = 2 },
             },
