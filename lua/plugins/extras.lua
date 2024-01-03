6a895e1ca82954106a9b5d89d35de4a6955ae309
Artem Vyshar <as.vyshar@gmail.com>
2024-01-03 12:40:47 +0200

    some upd

---
+++ lua/plugins/extras.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        'vimpostor/vim-tpipeline',
+        config = function()
+            vim.g.tpipeline_autoembed = 0
+
+        end
+    },
+    'christoomey/vim-tmux-navigator'
+}
---
+++ lua/plugins/lsp/css.lua
@@ -0,0 +1,7 @@
+local lspconfig = require 'lspconfig'
+lspconfig .cssmodules_ls.setup{}
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+lspconfig.cssls.setup {
+  capabilities = capabilities,
+}
