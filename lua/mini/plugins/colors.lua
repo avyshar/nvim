d0bc005c9306b129caeae37f87f78b4c2c496aad
Artem Vyshar <avyshar@turnitin.com>
2024-01-26 22:40:11 +0200

    new colorscheme

--- lua/mini/plugins/colors.lua
+++ lua/mini/plugins/colors.lua
@@ -11,7 +11,25 @@ return {
     priority = 1000,
     opts = {},
     config = function ()
-      vim.cmd[[colorscheme tokyonight]]
+      -- vim.cmd[[colorscheme tokyonight]]
+      -- vim.cmd("hi WinSeparator guifg=#0288d1")
+    end
+  },
+  {
+    'olivercederborg/poimandres.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('poimandres').setup {
+        -- leave this setup function empty for default config
+        -- or refer to the configuration section
+        -- for configuration options
+      }
+    end,
+
+    -- optionally set the colorscheme within lazy config
+    init = function()
+      vim.cmd("colorscheme poimandres")
       vim.cmd("hi WinSeparator guifg=#0288d1")
     end
   }
