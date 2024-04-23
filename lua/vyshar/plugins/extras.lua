94f4fcc6cb3c9e7fb4d3d8782ed58da2e229dee6
Artem Vyshar <avyshar@turnitin.com>
2024-04-23 18:16:05 +0300

    test 1 impr

--- lua/vyshar/plugins/extras.lua
+++ lua/vyshar/plugins/extras.lua
@@ -1,110 +1,168 @@
 return {
-    'tpope/vim-sleuth',
-    {
-        'numToStr/Comment.nvim',
-        config = function()
-            require('Comment').setup()
-        end,
+  'tpope/vim-sleuth',
+  {
+    'numToStr/Comment.nvim',
+    config = function()
+      require('Comment').setup()
+    end,
+  },
+  {
+    'folke/which-key.nvim',
+    event = 'VeryLazy',
+    lazy = true,
+    opts = {
+      plugins = {
+        marks = false, -- shows a list of your marks on ' and `
+        registers = false, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+        spelling = {
+          enabled = true,
+          suggestions = 20,
+        },
+        -- the presets plugin, adds help for a bunch of default keybindings in Neovim
+        -- No actual key bindings are created
+        presets = {
+          operators = false, -- adds help for operators like d, y, ...
+          motions = false, -- adds help for motions
+          text_objects = false, -- help for text objects triggered after entering an operator
+          windows = false, -- default bindings on <c-w>
+          nav = false, -- misc bindings to work with windows
+          z = false, -- bindings for folds, spelling and others prefixed with z
+          g = false, -- bindings for prefixed with g
+        },
+      },
+      operators = { gc = 'Comments' }, -- show the currently pressed key and its label as a message in the command line
+      icons = {
+        breadcrumb = '»', -- symbol used in the command line area that shows your active key combo
+        separator = require('vyshar.config.icons').ui.BoldArrowRight, -- symbol used between a key and it's label
+        group = '+', -- symbol prepended to a group
+      },
+      popup_mappings = {
+        scroll_down = '<c-d>', -- binding to scroll down inside the popup
+        scroll_up = '<c-u>', -- binding to scroll up inside the popup
+      },
+      window = {
+        border = 'single', -- none, single, double, shadow
+        position = 'bottom', -- bottom, top
+        margin = { 2, 0, 2, 0 }, -- extra window margin [top, right, bottom, left]
+        padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
+        winblend = 0,
+        zindex = 1000, -- positive value to position WhichKey above other floating windows.
+      },
+      layout = {
+        height = { min = 4, max = 25 }, -- min and max height of the columns
+        width = { min = 20, max = 50 }, -- min and max width of the columns
+        spacing = 3, -- spacing between columns
+        align = 'left', -- align columns left, center or right
+      },
+      ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
+      hidden = { '<silent>', '<cmd>', '<Cmd>', '<CR>', 'call', 'lua', '^:', '^ ' }, -- hide mapping boilerplate
+      show_help = true, -- show help message on the command line when the popup is visible
+      show_keys = true,
+      triggers = 'auto', -- automatically setup triggers
+      triggers_blacklist = {
+        i = { 'j', 'k' },
+        v = { 'j', 'k' },
+      },
+      -- Disabled by default for Telescope
+      disable = {
+        buftypes = {},
+        filetypes = { 'TelescopePrompt' },
+      },
     },
-    {
-        'folke/which-key.nvim',
-        event = 'VimEnter',
-        config = function()
-            require('which-key').setup()
+    config = function(_, opts)
+      local which_key = require 'which-key'
+      which_key.setup(opts)
+      which_key.register(require 'vyshar.config.which-key.defaults', {
+        mode = 'n',
+        prefix = '<leader>',
+      })
 
-            -- Document existing key chains
-            require('which-key').register {
-                ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-                ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-                ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-                ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-                ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-            }
-        end,
+      which_key.register(require 'vyshar.config.which-key.non_leader')
+    end,
+  },
+  { -- Autoformat
+    'stevearc/conform.nvim',
+    opts = {
+      notify_on_error = false,
+      format_on_save = function(bufnr)
+        -- Disable "format_on_save lsp_fallback" for languages that don't
+        -- have a well standardized coding style. You can add additional
+        -- languages here or re-enable it for the disabled ones.
+        local disable_filetypes = { c = true, cpp = true, sql = true }
+        return {
+          timeout_ms = 500,
+          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+        }
+      end,
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        -- Conform can also run multiple formatters sequentially
+        -- python = { "isort", "black" },
+        --
+        -- You can use a sub-list to tell conform to run *until* a formatter
+        -- is found.
+        javascript = { { 'prettierd', 'prettier' } },
+      },
     },
-    { -- Autoformat
-        'stevearc/conform.nvim',
-        opts = {
-            notify_on_error = false,
-            format_on_save = function(bufnr)
-                -- Disable "format_on_save lsp_fallback" for languages that don't
-                -- have a well standardized coding style. You can add additional
-                -- languages here or re-enable it for the disabled ones.
-                local disable_filetypes = { c = true, cpp = true, sql = true }
-                return {
-                    timeout_ms = 500,
-                    lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-                }
-            end,
-            formatters_by_ft = {
-                lua = { 'stylua' },
-                -- Conform can also run multiple formatters sequentially
-                -- python = { "isort", "black" },
-                --
-                -- You can use a sub-list to tell conform to run *until* a formatter
-                -- is found.
-                javascript = { { 'prettierd', 'prettier' } },
-            },
-        },
+  },
+  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+    'ggandor/flit.nvim',
+    enabled = true,
+    keys = function()
+      ---@type LazyKeys[]
+      local ret = {}
+      for _, key in ipairs { 'f', 'F', 't', 'T' } do
+        ret[#ret + 1] = { key, mode = { 'n', 'x', 'o' }, desc = key }
+      end
+      return ret
+    end,
+    opts = { labeled_modes = 'nx' },
+    dependencies = {
+      'ggandor/leap.nvim',
     },
-    { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
-    {
-        'ggandor/flit.nvim',
-        enabled = true,
-        keys = function()
-            ---@type LazyKeys[]
-            local ret = {}
-            for _, key in ipairs { 'f', 'F', 't', 'T' } do
-                ret[#ret + 1] = { key, mode = { 'n', 'x', 'o' }, desc = key }
-            end
-            return ret
-        end,
-        opts = { labeled_modes = 'nx' },
-        dependencies = {
-            'ggandor/leap.nvim',
-        },
+  },
+  -- {
+  --   'vimpostor/vim-tpipeline',
+  --   config = function()
+  --     vim.g.tpipeline_autoembed = 0
+  --     vim.g.tpipeline_clearstl = 1
+  --   end,
+  -- },
+  {
+    'utilyre/barbecue.nvim',
+    name = 'barbecue',
+    version = '*',
+    dependencies = {
+      'SmiteshP/nvim-navic',
+      'nvim-tree/nvim-web-devicons', -- optional dependency
     },
-    -- {
-    --   'vimpostor/vim-tpipeline',
-    --   config = function()
-    --     vim.g.tpipeline_autoembed = 0
-    --     vim.g.tpipeline_clearstl = 1
-    --   end,
-    -- },
-    {
-        'utilyre/barbecue.nvim',
-        name = 'barbecue',
-        version = '*',
-        dependencies = {
-            'SmiteshP/nvim-navic',
-            'nvim-tree/nvim-web-devicons', -- optional dependency
-        },
-        config = function()
-            local barbecue = require 'barbecue'
+    config = function()
+      local barbecue = require 'barbecue'
 
-            barbecue.setup {
-                theme = 'rose-pine',
-                attach_navic = false,
-                show_dirname = true,
-                show_basename = true,
-            }
-        end,
+      barbecue.setup {
+        theme = 'rose-pine',
+        attach_navic = false,
+        show_dirname = true,
+        show_basename = true,
+      }
+    end,
+  },
+  {
+    'christoomey/vim-tmux-navigator',
+    cmd = {
+      'TmuxNavigateLeft',
+      'TmuxNavigateDown',
+      'TmuxNavigateUp',
+      'TmuxNavigateRight',
+      'TmuxNavigatePrevious',
     },
-    {
-        'christoomey/vim-tmux-navigator',
-        cmd = {
-            'TmuxNavigateLeft',
-            'TmuxNavigateDown',
-            'TmuxNavigateUp',
-            'TmuxNavigateRight',
-            'TmuxNavigatePrevious',
-        },
-        keys = {
-            { '<c-h>',  '<cmd><C-U>TmuxNavigateLeft<cr>' },
-            { '<c-j>',  '<cmd><C-U>TmuxNavigateDown<cr>' },
-            { '<c-k>',  '<cmd><C-U>TmuxNavigateUp<cr>' },
-            { '<c-l>',  '<cmd><C-U>TmuxNavigateRight<cr>' },
-            { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
-        },
+    keys = {
+      { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
+      { '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
+      { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
+      { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
+      { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
     },
+  },
 }
--- lua/vyshar/plugins/telescope.lua
+++ lua/vyshar/plugins/telescope.lua
@@ -1,105 +1,189 @@
 return {
-  { -- Fuzzy Finder (files, lsp, etc)
+  {
     'nvim-telescope/telescope.nvim',
-    event = 'VimEnter',
-    branch = '0.1.x',
+    cmd = 'Telescope',
+    version = false,
+    lazy = true,
     dependencies = {
       'nvim-lua/plenary.nvim',
-      { -- If encountering errors, see telescope-fzf-native README for installation instructions
-        'nvim-telescope/telescope-fzf-native.nvim',
-
-        -- `build` is used to run some command when the plugin is installed/updated.
-        -- This is only run then, not every time Neovim starts up.
-        build = 'make',
-
-        -- `cond` is a condition used to determine whether this plugin should be
-        -- installed and loaded.
-        cond = function()
-          return vim.fn.executable 'make' == 1
-        end,
-      },
-      { 'nvim-telescope/telescope-ui-select.nvim' },
-
-      -- Useful for getting pretty icons, but requires a Nerd Font.
-      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
-      { 'MunifTanjim/nui.nvim' },
+      'nvim-tree/nvim-web-devicons',
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+      'nvim-telescope/telescope-ui-select.nvim',
+      -- "telescope-dap.nvim",
+      'kkharji/sqlite.lua',
+      -- "nvim-telescope/telescope-frecency.nvim",
     },
     config = function()
-      require('telescope').setup {
+      local telescope = require 'telescope'
+      local actions = require 'telescope.actions'
+      -- local trouble = require("trouble.providers.telescope")
+      local icons = require 'vyshar.config.icons'
+
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = 'TelescopeResults',
+        callback = function(ctx)
+          vim.api.nvim_buf_call(ctx.buf, function()
+            vim.fn.matchadd('TelescopeParent', '\t\t.*$')
+            vim.api.nvim_set_hl(0, 'TelescopeParent', { link = 'Comment' })
+          end)
+        end,
+      })
+
+      local function formattedName(_, path)
+        local tail = vim.fs.basename(path)
+        local parent = vim.fs.dirname(path)
+        if parent == '.' then
+          return tail
+        end
+        return string.format('%s\t\t%s', tail, parent)
+      end
+
+      telescope.setup {
+        file_ignore_patterns = { '%.git/.' },
+        -- borderchars = { "█", " ", "▀", "█", "█", " ", " ", "▀" },
         defaults = {
-          -- Default configuration for telescope goes here:
-          -- config_key = value,
-          layout_strategy = 'horizontal',
+          mappings = {
+            i = {
+              ['<esc>'] = actions.close,
+              -- ["<C-t>"] = trouble.open_with_trouble,
+            },
+
+            n = {
+              -- ["<C-t>"] = trouble.open_with_trouble
+            },
+          },
+          previewer = false,
+          prompt_prefix = ' ' .. icons.ui.Telescope .. ' ',
+          selection_caret = icons.ui.BoldArrowRight .. ' ',
+          file_ignore_patterns = { 'node_modules', 'package-lock.json' },
+          initial_mode = 'insert',
+          select_strategy = 'reset',
+          sorting_strategy = 'ascending',
+          color_devicons = true,
+          set_env = { ['COLORTERM'] = 'truecolor' }, -- default = nil,
           layout_config = {
-            horizontal = {
-              prompt_position = 'top',
-              preview_width = 0.55,
-              results_width = 0.8,
-            },
-            vertical = {
-              mirror = false,
-            },
-            width = 0.87,
-            height = 0.80,
+            prompt_position = 'top',
             preview_cutoff = 120,
           },
-          border = {},
-          borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
-        },
-        pickers = {
-          -- Default configuration for builtin pickers goes here:
-          -- picker_name = {
-          --   picker_config_key = value,
-          --   ...
-          -- }
-          -- Now the picker_config_key will be applied every time you call this
-          -- builtin picker
-        },
-        extensions = {
-          ['ui-select'] = {
-            require('telescope.themes').get_dropdown(),
+          vimgrep_arguments = {
+            'rg',
+            '--color=never',
+            '--no-heading',
+            '--with-filename',
+            '--line-number',
+            '--column',
+            '--smart-case',
+            '--hidden',
+            '--glob=!.git/',
           },
         },
+        pickers = {
+          find_files = {
+            previewer = false,
+            path_display = formattedName,
+            layout_config = {
+              height = 0.4,
+              prompt_position = 'top',
+              preview_cutoff = 120,
+            },
+          },
+          git_files = {
+            previewer = false,
+            path_display = formattedName,
+            layout_config = {
+              height = 0.4,
+              prompt_position = 'top',
+              preview_cutoff = 120,
+            },
+          },
+          buffers = {
+            path_display = formattedName,
+            mappings = {
+              i = {
+                ['<c-d>'] = actions.delete_buffer,
+              },
+              n = {
+                ['<c-d>'] = actions.delete_buffer,
+              },
+            },
+            previewer = false,
+            initial_mode = 'normal',
+            -- theme = "dropdown",
+            layout_config = {
+              height = 0.4,
+              width = 0.6,
+              prompt_position = 'top',
+              preview_cutoff = 120,
+            },
+          },
+          current_buffer_fuzzy_find = {
+            previewer = true,
+            layout_config = {
+              prompt_position = 'top',
+              preview_cutoff = 120,
+            },
+          },
+          live_grep = {
+            only_sort_text = true,
+            previewer = true,
+          },
+          grep_string = {
+            only_sort_text = true,
+            previewer = true,
+          },
+          lsp_references = {
+            show_line = false,
+            previewer = true,
+          },
+          treesitter = {
+            show_line = false,
+            previewer = true,
+          },
+          colorscheme = {
+            enable_preview = true,
+          },
+        },
+        extensions = {
+          fzf = {
+            fuzzy = true, -- false will only do exact matching
+            override_generic_sorter = true, -- override the generic sorter
+            override_file_sorter = true, -- override the file sorter
+            case_mode = 'smart_case', -- or "ignore_case" or "respect_case"
+          },
+          ['ui-select'] = {
+            require('telescope.themes').get_dropdown {
+              previewer = false,
+              initial_mode = 'normal',
+              sorting_strategy = 'ascending',
+              layout_strategy = 'horizontal',
+              layout_config = {
+                horizontal = {
+                  width = 0.5,
+                  height = 0.4,
+                  preview_width = 0.6,
+                },
+              },
+            },
+          },
+          -- frecency = {
+          --   default_workspace = "CWD",
+          --   show_scores = true,
+          --   show_unindexed = true,
+          --   disable_devicons = false,
+          --   ignore_patterns = {
+          --     "*.git/*",
+          --     "*/tmp/*",
+          --     "*/lua-language-server/*",
+          --   },
+          -- },
+        },
       }
-      -- Enable Telescope extensions if they are installed
-      pcall(require('telescope').load_extension, 'fzf')
-      pcall(require('telescope').load_extension, 'ui-select')
-
-      -- See `:help telescope.builtin`
-      local builtin = require 'telescope.builtin'
-      vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-      vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-      vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-      vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-      vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-      vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-
-      -- Slightly advanced example of overriding default behavior and theme
-      vim.keymap.set('n', '<leader>/', function()
-        -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-        builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-          winblend = 10,
-          previewer = false,
-        })
-      end, { desc = '[/] Fuzzily search in current buffer' })
-
-      -- It's also possible to pass additional configuration options.
-      --  See `:help telescope.builtin.live_grep()` for information about particular keys
-      vim.keymap.set('n', '<leader>s/', function()
-        builtin.live_grep {
-          grep_open_files = true,
-          prompt_title = 'Live Grep in Open Files',
-        }
-      end, { desc = '[S]earch [/] in Open Files' })
-
-      -- Shortcut for searching your Neovim configuration files
-      vim.keymap.set('n', '<leader>sn', function()
-        builtin.find_files { cwd = vim.fn.stdpath 'config' }
-      end, { desc = '[S]earch [N]eovim files' })
+      telescope.load_extension 'fzf'
+      telescope.load_extension 'ui-select'
+      -- telescope.load_extension("refactoring")
+      -- telescope.load_extension("dap")
+      -- telescope.load_extension("frecency")
+      -- telescope.load_extension("notify")
     end,
   },
 }
