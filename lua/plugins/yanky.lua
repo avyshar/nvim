5e2cccd671f817f4e80a89108571f7ca37ebafc0
Artem Vyshar <avyshar@turnitin.com>
2024-05-20 14:52:38 +0300

    yanky upd

--- lua/plugins/yanky.lua
+++ lua/plugins/yanky.lua
@@ -1,8 +1,18 @@
 return {
   "gbprod/yanky.nvim",
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
+  config = function()
+    require("yanky").setup({
+      ring = {
+        history_length = 100,
+        storage = "shada",
+        sync_with_numbered_registers = true,
+        cancel_event = "update",
+        ignore_registers = { "_" },
+        update_register_on_cycle = false,
+      },
+      system_clipboard = {
+        sync_with_ring = true,
+      },
+    })
+  end,
 }
